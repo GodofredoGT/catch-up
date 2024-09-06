c668c01db7fb1d65d1b0da87868528e66958b341
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-09-06 16:51:55 -0500

    feat(news): add unavailable content component.

---
+++ src/news/components/unavailable-content.component.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "unavailable-content",
+  props:{errors:null}
+}
+</script>
+
+<template>
+  <!--translation es la 't' que es de vue--->
+<div><h4>{{$t('unavailable-news')}}</h4></div>
+  <div v-for="error in errors" :key="error">
+    <h6>{{error}}</h6>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
